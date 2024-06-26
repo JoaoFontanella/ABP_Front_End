1cf3d92182bf7f778d79195d51dc0d2aff8f2e9f
RenanMonteiroTomaz <rmon123t@gmail.com>
2024-06-25 22:26:59 -0300

    Carrinho funcional

--- twstore/src/Cart.jsx
+++ twstore/src/Cart.jsx
@@ -1,45 +1,47 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import './styles/Cart.css';
+import { CartContext } from './CartContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './styles/Cart.css';
 import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  const [cartItems, setCartItems] = useState([
-    { id: 1, name: 'Camiseta Grizzly Evergreen Ss Tee', price: 149.9 },
-    { id: 2, name: 'Camiseta Vans Marshmallow White', price: 104.99 },
-    { id: 3, name: 'Calça Hocks Fixa Large Jeans', price: 299.9 },
-  ]);
-  //array de teste para o carrinho
+  const { cartItems, removeFromCart, clearCart } = useContext(CartContext);
 
-  const removeItem = (id) => {
-    setCartItems(cartItems.filter((item) => item.id !== id));
-  };
-
-  const calculateTotal = () => {
-    return cartItems.reduce((total, item) => total + item.price, 0);
-  };
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
 
   return (
-    <div className="cart-container">
+    <div className="cart">
       <Header />
-      <div className="cart-content">
-        <h2>Carrinho</h2>
-        <ul>
-          {cartItems.map((item) => (
-            <li key={item.id}>
-              {item.name} - R$ {item.price.toFixed(2)}
-              <button onClick={() => removeItem(item.id)}>Remover</button>
-            </li>
-          ))}
-        </ul>
-        <p>Total: R$ {calculateTotal().toFixed(2)}</p>
-      </div>
-      <Link to="/Checkout" id="chkt">
-        <button id="chkt" type="submit">
-          Checkout
-        </button>
-      </Link>
+      <h2>Carrinho de Compras</h2>
+      {cartItems.length === 0 ? (
+        <p>O carrinho está vazio.</p>
+      ) : (
+        <>
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id}>
+                {item.title} - ${item.price} - Quantidade: {item.quantity}
+                <button onClick={() => removeFromCart(item.id)}>Remover</button>
+              </li>
+            ))}
+          </ul>
+          <p>Total: ${totalPrice.toFixed(2)}</p>
+          <div class="cart-buttons">
+            <button id="clean" onClick={clearCart}>
+              Limpar
+            </button>
+            <Link to="/Checkout">
+              <button id="chk" type="submit">
+                Checkout
+              </button>
+            </Link>
+          </div>
+        </>
+      )}
       <Footer />
     </div>
   );
